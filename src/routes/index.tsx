38c426112fa4de3ba5c67d25ebd128e85ced5bf6
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-07-12 18:59:19 -0300

    Seccion 2: Empezamos a usar Signal

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,30 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const pokemonId = useSignal(1); // Usarlo solo para valores primitivos (boolean, string, number)
+  // const pokemonId = useStore(); // Usarlo solo para valores compuestos
+
   return (
-    <div class="text-center">
-      <span class="text-5xl">Hola Mundo</span>
+    <div class='flex flex-col text-center'>
+      <span class='text-2xl'>Buscador simple</span>
+      <span class='text-9xl'>{pokemonId}</span>
+
+      {/** TODO: Crear imagen */}
+      <div class='mt2'>
+        <button class='btn btn-primary mr-2'>Anterior</button>
+        <button class='btn btn-primary'>Siguiente</button>
+      </div>
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Poke Qwik",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Esta es mi primera aplicación en Qwik",
     },
   ],
 };
