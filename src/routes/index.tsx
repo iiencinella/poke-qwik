1f91b7c625a1ed88fe2d11391ebbb38d3b4391f4
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-07-13 11:19:19 -0300

    Sección 2: Modificamos el valor del Signal

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,33 @@
-import { component$, useSignal, useStore } from "@builder.io/qwik";
+import { $, component$, useSignal, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   const pokemonId = useSignal(1); // Usarlo solo para valores primitivos (boolean, string, number)
   // const pokemonId = useStore(); // Usarlo solo para valores compuestos
 
+  // Para poder manipular Signals, la función debe de estar serializada de la siguiente manera $([función])
+  const changePokemonId = $((value:number) => {
+    if(pokemonId.value + value <= 0) return; //Evaluamos que el valor del Signal no sea menor a 0
+    if(pokemonId.value + value > 1010) return; //Evaluamos que el valor del Signal no sea mayor a la cantidad de pokemons existentes (este control lo hacemos manual)
+
+    pokemonId.value += value;
+  })
+
   return (
     <div class='flex flex-col justify-center items-center'>
       <span class='text-2xl'>Buscador simple</span>
       <span class='text-9xl'>{pokemonId}</span>
 
-      <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId.value}.png`}
+      <img 
+        width={64}
+        height={64}
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId.value}.png`}
         alt="Pokemon Sprite"
         style={{ width: '200px' }} />
 
       <div class='mt2'>
-        <button onClick$={() => pokemonId.value--} class='btn btn-primary mr-2'>Anterior</button>
-        <button onClick$={() => pokemonId.value++} class='btn btn-primary'>Siguiente</button>
+        <button onClick$={() => changePokemonId(-1)} class='btn btn-primary mr-2'>Anterior</button>
+        <button onClick$={() => changePokemonId(1)} class='btn btn-primary'>Siguiente</button>
       </div>
     </div>
   );
