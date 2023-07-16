4f84653f24e2f13f670198e7551e5e39974da03c
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-07-16 19:02:41 -0300

    Sección 3: Modificamos el tab

--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,7 +1,18 @@
 import { component$ } from '@builder.io/qwik';
+import { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
     <>Hola Mundo - List SSR</>
   )
 });
+
+export const head: DocumentHead = {
+  title: "Poke Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Esta es mi primera aplicación en Qwik",
+    },
+  ],
+};
