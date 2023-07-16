7dcc1fe25c6794487e2c9396282ae3914e81c7fa
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-07-16 19:50:05 -0300

    Sección 3: usamos routerHooks

--- src/routes/pokemon/[id]/index.tsx
+++ src/routes/pokemon/[id]/index.tsx
@@ -1,15 +1,28 @@
 import { component$ } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
+import { routeLoader$ } from '@builder.io/qwik-city';
 import { PokemonImage } from '~/components/pokemons/pokemon-image';
 
+//Este hook lo vamos a poder usar para realizar validaciones antes de que se cargue la pantalla
+export const usePokemonId = routeLoader$<number>(({ params, redirect}) => {
+  const id = Number(params.id);
+
+  // Evaluamos que el id sea numérico  esté dentro del rango de la cantidad de pokemons.
+  // En caso de que no cumpla alguna condición, será redireccionado a la página de inicio
+  if (isNaN(id)) redirect( 301, '/');
+  if (id <= 0) redirect( 301, '/');
+  if (id > 1010) redirect( 301, '/');
+
+  return id;
+})
+
 export default component$(() => {
-  const location = useLocation();
+  const pokemonId = usePokemonId();
 
   return (
     <div class='flex justify-center items-center flex-col'>
-      <span class='text-5xl'>Pokemon: {location.params.id}</span>
+      <span class='text-5xl'>Pokemon: {pokemonId}</span>
 
-      <PokemonImage id={location.params.id} />
+      <PokemonImage id={pokemonId.value} />
     </div>
   )
 });
