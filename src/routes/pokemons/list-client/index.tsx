3cf950201156ec6688c847e23178a6a1ad527411
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-07-17 00:39:29 -0300

    Sección 3: Empezamos a usar useStore

--- src/routes/pokemons/list-client/index.tsx
+++ src/routes/pokemons/list-client/index.tsx
@@ -1,9 +1,42 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStore } from '@builder.io/qwik';
 import { type DocumentHead } from '@builder.io/qwik-city';
+import { SmallPokemon } from '~/interfaces';
+
+interface PokemonPageState {
+  currentPage: number;
+  pokemons: SmallPokemon[];
+}
 
 export default component$(() => {
+  const pokemonState = useStore<PokemonPageState>({
+    currentPage: 0,
+    pokemons: []
+  });
+  
   return (
-    <>Hola Mundo - List Client</>
+    <div class='flex flex-col justify-center items-center'>
+      <div class='flex justify-center items-center flex-col'>
+        <span class='my-5 text-5xl'>Status</span>
+        <span>Página actual: {pokemonState.currentPage}</span>
+        <span>Está cargando: </span>
+      </div>
+
+      <div class="mt-10">
+        <button onClick$={() => pokemonState.currentPage--} class='btn btn-primary mr-2'>Anterior</button>
+        <button onClick$={() => pokemonState.currentPage++} class='btn btn-primary'>Siguiente</button>
+      </div>
+
+      <div class="grid grid-cols-6 mt-5">
+        {/* {
+          pokemons.value.map(({ name, id }) => (
+            <div key={name} class="m-5 flex flex-col justify-center items-center">
+              <PokemonImage id={id} />
+              <span class='capitalize'>{name}</span>
+            </div>
+          ))
+        } */}
+      </div>
+    </div>
   )
 });
 
