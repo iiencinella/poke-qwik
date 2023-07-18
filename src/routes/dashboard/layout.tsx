70250a5186cc51341b928079afe745e43e5c648a
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-07-18 20:32:18 -0300

    Sección 7: Validamos existencia de cookie

--- src/routes/dashboard/layout.tsx
+++ src/routes/dashboard/layout.tsx
@@ -1,4 +1,17 @@
 import { Slot, component$ } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+
+// Validamos si el usuario inició sesión, caso contrario se manda a la página de inicio de sesión
+export const useCheckAuthCookie = routeLoader$(({cookie, redirect}) => {
+  const jwtCookie = cookie.get('jwt');
+
+  if(jwtCookie) {
+    console.log('Cookie válido: ', jwtCookie);
+    return 
+  }
+
+  redirect(302, '/login');
+});
 
 export default component$(() => {
   return (
