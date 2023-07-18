de38772348397ee33158157ac2ab54c0131ee9fc
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-07-18 19:50:17 -0300

    Sección 7: Agregamos validaciones manuales

--- src/routes/(auth)/login/index.tsx
+++ src/routes/(auth)/login/index.tsx
@@ -1,5 +1,5 @@
 
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { $, component$, useComputed$, useStore, useStylesScoped$ } from '@builder.io/qwik';
 
 import styles from './login.css?inline';
 
@@ -7,24 +7,65 @@ export default component$(() => {
 
   useStylesScoped$(styles);
 
+  const formState = useStore({
+    email: '',
+    password: '',
+    formPosted: false
+  });
+
+  const emailError = useComputed$(() => {
+    if (formState.email.includes('@')) return '';
+
+    return 'not-valid';
+  });
+
+  const passwordError = useComputed$(() => {
+    if (formState.password.length >= 6) return '';
+
+    return 'not-valid';
+  });
+
+  const isFormValid = useComputed$(() => {
+    if(emailError.value === 'not-valid' || passwordError.value === 'not-valid') return false;
+
+    return true;
+  });
+
+  const onSubmit = $(() => {
+    formState.formPosted = true;
+    const { email, password } = formState;
+  });
+
   return (
-    <form class="login-form">
+    <form onSubmit$={onSubmit} class="login-form" preventdefault: submit>
       <div class="relative">
-        <input name="email" type="text" placeholder="Email address" />
+        <input
+          value={formState.email}
+          onInput$={(ev) => formState.email = (ev.target as HTMLInputElement).value}
+          name="email"
+          type="text"
+          class={formState.formPosted ? emailError : ''}
+          placeholder="Email address" />
         <label for="email">Email Address</label>
       </div>
       <div class="relative">
-        <input id="password" name="password" type="password" placeholder="Password" />
+        <input
+          value={formState.password}
+          onInput$={(ev) => formState.password = (ev.target as HTMLInputElement).value}
+          name="password"
+          type="password"
+          class={formState.formPosted ? passwordError : ''}
+          placeholder="Password" />
         <label for="password">Password</label>
       </div>
       <div class="relative">
-        <button>Ingresar</button>
+        <button type='submit'>Ingresar</button>
       </div>
 
 
-      {/* <code>
-                { JSON.stringify( formState, undefined , 2 ) }
-            </code> */}
+      <code>
+        {JSON.stringify(formState, undefined, 2)}
+      </code>
     </form>
   )
 });
