dc5e2131d8eae025b1e66e843e02a5ff87faaa4b
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-07-17 22:32:33 -0300

    Seccion 4: Leemos del localstorage

--- src/context/pokemon/pokemon-provider.tsx
+++ src/context/pokemon/pokemon-provider.tsx
@@ -19,10 +19,18 @@ export const PokemonProvider = component$(() => {
   useContextProvider(PokemonListContext, pokemonList);
 
   useVisibleTask$(() => {
+    // Aqui vamos a leer infromacion desde el localstorage
+    if(localStorage.getItem('pokemon-game')) {
+      const {pokemonId = 10, isPokemonVisible = true, showBackImage = false} = JSON.parse(localStorage.getItem('pokemon-game')!) as PokemonGameState;
 
+      pokemonGame.isPokemonVisible = isPokemonVisible;
+      pokemonGame.pokemonId = pokemonId;
+      pokemonGame.showBackImage = showBackImage;
+    }
   });
 
   useVisibleTask$(({ track }) => {
+    // Aqui vamos a guardar infromacion al localstorage
     track(() => [pokemonGame.isPokemonVisible, pokemonGame.pokemonId, pokemonGame.showBackImage]);
 
     localStorage.setItem('pokemon-game', JSON.stringify(pokemonGame))
